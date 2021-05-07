5fa146a4ee451cf962141d6e3135dadec103762c
Muhammad_Eka_Bakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2021-05-07 14:01:46 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4046/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4046/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if4046.galerihewan.model
 
+import org.d3if4046.galerihewan.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val iamgeResId: Int
+    val iamgeResId: Int = R.drawable.angsa
 )
